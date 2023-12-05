8941335762e238b1dc76a4eed72e8b2f6faa3a9b
samuel <samuelayano7@gmail.com>
2023-12-05 11:46:28 +0100

    1-dart_learn.dart

---
+++ 1-dart_learn.dart
@@ -0,0 +1,34 @@
+//datatypes in dart
+void main() {
+  // Var
+  var name = "samuel";
+  print("Var:$name");
+
+  // Const
+  const String firstname = "John";
+  print("Const:$firstname");
+
+  // Final
+  final String lastname = "Smith";
+  print("Final:$lastname");
+
+  // String
+  String username = "onaya";
+  print("String:$username");
+
+  // Integer
+  int age = 30;
+  print("Int:$age");
+
+  // Dynamic
+  dynamic guess = "pavillion";
+  print("Dynamic:$guess");
+
+  // Declearing but not using
+  var car;
+  int model;
+  car = "Lexus";
+  print(car);
+  model = 20;
+  print(model);
+}
