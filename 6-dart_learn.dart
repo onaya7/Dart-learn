be7c08a9bc368ef64a4ae49b1e6f11666e4208cc
samuel <samuelayano7@gmail.com>
2023-12-07 19:00:23 +0100

    6-dart_learn.dart

--- 6-dart_learn.dart
+++ 6-dart_learn.dart
@@ -2,11 +2,12 @@ void main() {
   // Functions
   // A function is a block of code that performs a specific task
   // A function is executed when it is called
-  new_func() {
-    var name = "Hello World";
-    return name;
+  new_func(String name, int age, String occupation, var status) {
+    String result =
+        "My name is $name and my age is $age and myoccupation is $occupation and i am also $status";
+    return result;
   }
 
-  var result = new_func();
+  var result = new_func("samuel", 30, "Software Engineer", "employed");
   print(result);
 }
