22cb3e1258ce8e5ebc2003058d6228a5341beca8
samuel <samuelayano7@gmail.com>
2023-12-07 13:22:13 +0100

    6-dart_learn.dart

--- 6-dart_learn.dart
+++ 6-dart_learn.dart
@@ -1,5 +1,12 @@
 void main() {
   // Functions
   // A function is a block of code that performs a specific task
-  
+  // A function is executed when it is called
+  new_func() {
+    var name = "Hello World";
+    return name;
+  }
+
+  var result = new_func();
+  print(result);
 }
